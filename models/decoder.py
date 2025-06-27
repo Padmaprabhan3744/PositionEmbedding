84c6c8748174846d660f6fe83757bbbbdd4ddd3c
Padmaprabhan3744 <me22btech11001@iith.ac.in>
2025-06-27 14:12:56 +0530

    Update decoder.py

--- models/decoder.py
+++ models/decoder.py
@@ -2,6 +2,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model, max_len=5000):
+        super(PositionalEmbedding, self).__init__()
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+
 class DecoderLayer(nn.Module):
     def __init__(self, self_attention, cross_attention, d_model, d_ff=None,
                  dropout=0.1, activation="relu"):
@@ -18,8 +39,11 @@ class DecoderLayer(nn.Module):
         self.activation = F.relu if activation == "relu" else F.gelu
 
     def forward(self, x, cross, x_mask=None, cross_mask=None):
+        device=x.device
+        pos=PositionEmbedding(self.d_model)(x).to(device)
+        pos=pos.expand(x.shape[0],-1,-1)
         x = x + self.dropout(self.self_attention(
-            x, x, x,
+            pos, pos, x,
             attn_mask=x_mask
         )[0])
         x = self.norm1(x)
